4fe707591e89a26f1c59ef6110b3b2329fa27fc4
hushiooo <joad.goutal@stoik.io>
2025-06-17 17:26:09 +0100

    go back to unstable nixpkgs branch

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "My workstation nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-25.05";
+      url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
